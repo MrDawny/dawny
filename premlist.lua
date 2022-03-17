1a9bfe164a14ccc6ce33e7ad494b09fd280b060a
random <78721749+LPrandom@users.noreply.github.com>
2022-03-16 18:08:41 -1000

    Update premlist.lua

--- premlist.lua
+++ premlist.lua
@@ -1,1 +1,14 @@
+local ModIDs = {}
 
+local success, err = pcall(function()
+    for i,v in pairs(game.Players:GetChildren()) do
+        if v.Character
+            if table.find(ModIDs,v.UserId) then
+                v.Character.Humanoid.DisplayName = "[😎]"..v.DisplayName
+            elseif not v.Character.Head:FindFirstChild("OriginalSize") then
+                v.Character.Humanoid.DisplayName = "[🤡]"..v.DisplayName
+            end
+        end
+    end
+end)
+return ModIDs
